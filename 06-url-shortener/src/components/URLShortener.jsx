d91297cb7b7427cbcb09da0a4ec67b73e8475cee
mamatsa <otar.mamatsashvili@gmail.com>
2023-11-30 17:11:56 +0400

    06 feat: add error message

--- 06-url-shortener/src/components/URLShortener.jsx
+++ 06-url-shortener/src/components/URLShortener.jsx
@@ -6,20 +6,35 @@ import { useState } from "react";
 const URLShortener = () => {
   const [shortenedURLs, shortenUrl] = useShortURL();
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const submitHandler = (event) => {
     event.preventDefault();
     const inputURL = event.target.url.value;
-    setLoading(true);
-    shortenUrl(inputURL).then(() => {
-      setLoading(false);
-    });
+
+    if (!inputURL) {
+      setError("Please add a link");
+    } else {
+      setError("");
+      setLoading(true);
+      shortenUrl(inputURL).then(() => {
+        setLoading(false);
+      });
+    }
   };
 
   return (
     <div className={styles.container}>
       <form className={styles.form} onSubmit={submitHandler}>
-        <input type="text" name="url" placeholder="Shorten a link here..." />
+        <div className={styles.inputContainer}>
+          <input
+            type="text"
+            name="url"
+            className={error ? styles.error : ""}
+            placeholder="Shorten a link here..."
+          />
+          {error && <p className={styles.errorText}>{error}</p>}
+        </div>
         <button type="submit" disabled={loading}>
           {loading ? "Wait..." : "Shorten It!"}
         </button>
