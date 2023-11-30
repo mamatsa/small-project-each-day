c31c38f5f3d3224dcf9e4c3e488fa379a4e1c1c3
mamatsa <otar.mamatsashvili@gmail.com>
2023-11-30 15:41:04 +0400

    06 feat: shorten urls with api request

--- 06-url-shortener/src/components/URLShortener.jsx
+++ 06-url-shortener/src/components/URLShortener.jsx
@@ -1,12 +1,33 @@
 import styles from "styles/URLShortener.module.css";
+import useShortURL from "src/hooks/useShortURL";
 
 const URLShortener = () => {
+  const [shortenedURLs, shortenUrl] = useShortURL();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const inputURL = event.target.url.value;
+    shortenUrl(inputURL);
+  };
+  console.log(shortenedURLs);
+
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
-        <input type="text" placeholder="Shorten a link here..." />
-        <button>Shorten It!</button>
+      <form className={styles.form} onSubmit={submitHandler}>
+        <input type="text" name="url" placeholder="Shorten a link here..." />
+        <button type="submit">Shorten It!</button>
       </form>
+
+      <div>
+        {Object.entries(shortenedURLs).map((item) => {
+          return (
+            <div key={item[0]}>
+              <h3>{item[0]}</h3>
+              <p>{item[1]}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
---
+++ 06-url-shortener/src/hooks/useShortURL.jsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { useState } from "react";
+
+const generateRandomAlias = () => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+
+  while (result.length < 8) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+};
+
+function useShortURL() {
+  const [shortenedURLs, setShortenedURLs] = useState({});
+  const shortenUrl = async (inputUrl) => {
+    if (shortenedURLs[inputUrl]) {
+      return shortenedURLs[inputUrl];
+    }
+
+    const options = {
+      method: "POST",
+      url: "https://url-shortener23.p.rapidapi.com/shorten",
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "9d67a4daa0mshd63ae851c2bf538p1a1ae2jsnf6390d4d46b2",
+        "X-RapidAPI-Host": "url-shortener23.p.rapidapi.com",
+      },
+      data: {
+        url: inputUrl,
+        alias: generateRandomAlias(),
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      setShortenedURLs({
+        ...shortenedURLs,
+        [inputUrl]: response.data.short_url,
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return [shortenedURLs, shortenUrl];
+}
+
+export default useShortURL;
