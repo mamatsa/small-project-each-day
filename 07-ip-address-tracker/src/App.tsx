fc087250005c38981f450f044163ac77ff3cc3b1
mamatsa <otar.mamatsashvili@gmail.com>
2023-12-02 12:16:24 +0400

    07 feat: create basic visual

--- 07-ip-address-tracker/src/App.tsx
+++ 07-ip-address-tracker/src/App.tsx
@@ -1,5 +1,58 @@
+import { IconArrow } from "./components";
+import styles from "./styles/App.module.css";
+
 function App() {
-  return <h1>IP Address Tracker</h1>;
+  return (
+    <div className={styles.app}>
+      {/* Top Section */}
+      <div className={styles.topSection}>
+        <h1>IP Address Tracker</h1>
+
+        {/* Input field */}
+        <form action="" className={styles.mainForm}>
+          <input
+            type="text"
+            placeholder="Search for any IP address or domain"
+          />
+          <button>
+            <IconArrow />
+          </button>
+        </form>
+
+        {/* Result data */}
+        <div className={styles.infoContainer}>
+          <div>
+            <p>IP ADDRESS</p>
+            <h2>127.0.0.1</h2>
+          </div>
+          <div className={styles.middleInfo}>
+            <div className={styles.splitLine}></div>
+            <div>
+              <p>LOCATION</p>
+              <h2>Tbilisi, Georgia</h2>
+            </div>
+          </div>
+          <div className={styles.middleInfo}>
+            <div className={styles.splitLine}></div>
+            <div>
+              <p>TIMEZONE</p>
+              <h2>UTC-04:00</h2>
+            </div>
+          </div>
+          <div className={styles.middleInfo}>
+            <div className={styles.splitLine}></div>
+            <div>
+              <p>ISP</p>
+              <h2>Silknet</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Map Section */}
+      <div className={styles.map}></div>
+    </div>
+  );
 }
 
 export default App;
