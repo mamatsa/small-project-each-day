47d9f4f6dc8ce17e9d5aba222ab54413419347d2
mamatsa <otar.mamatsashvili@gmail.com>
2023-12-03 13:56:29 +0400

    08 chore: initialize new project

---
+++ 08-multi-step-form/src/components/svgs/index.ts
@@ -0,0 +1,5 @@
+export { default as IconArcade } from "./IconArcade";
+export { default as IconAdvanced } from "./IconAdvanced";
+export { default as IconCheckmark } from "./IconCheckmark";
+export { default as IconPron } from "./IconPron";
+export { default as IconThankYou } from "./IconThankYou";
---
+++ 08-multi-step-form/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = import("tailwindcss/defaultTheme");
+
+export default {
+  content: ["./index.html", "./src/**/*.tsx"],
+  theme: {
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
+    colors: {
+      // Primary
+      "marine-blue": "hsl(213, 96%, 18%)",
+      "purplish-blue": "hsl(243, 100%, 62%)",
+      "pastel-blue": "hsl(228, 100%, 84%)",
+      "light-blue": "hsl(206, 94%, 87%)",
+      "strawberry-red": "hsl(354, 84%, 57%)",
+      // Neutral
+      "cool-gray": "hsl(231, 11%, 63%)",
+      "light-gray": "hsl(229, 24%, 87%)",
+      magnolia: "hsl(217, 100%, 97%)",
+      alabaster: "hsl(231, 100%, 99%)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
