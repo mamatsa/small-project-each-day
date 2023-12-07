e0da65a54e289b7c4ccb87aa7a27a25c2df8b39e
mamatsa <otar.mamatsashvili@gmail.com>
2023-12-07 13:06:38 +0400

    08 style: style thank you section

--- 08-multi-step-form/src/components/step-sections/ThankYou.tsx
+++ 08-multi-step-form/src/components/step-sections/ThankYou.tsx
@@ -1,9 +1,18 @@
 import { Section } from "./components";
+import { IconThankYou } from "components";
 
 const ThankYou = () => {
   return (
     <Section>
-      <h1 className="text-marine-blue text-2xl font-bold">Thank You!</h1>
+      <div className="flex h-full w-full flex-col items-center justify-center gap-2 px-6 py-14">
+        <IconThankYou />
+        <h1 className="mt-5 text-2xl font-bold text-marine-blue">Thank You!</h1>
+        <p className="text-center leading-6 text-cool-gray">
+          Thanks for confirming your subscription! We hope you have fun using
+          our platform. If you ever need support, please feel free to email us
+          at support@loremgaming.com.
+        </p>
+      </div>
     </Section>
   );
 };
