b69dbd7883bb4a963c4d19c45e436eef6e04c2e0
mamatsa <otar.mamatsashvili@gmail.com>
2023-12-08 13:17:39 +0400

    09 chore: initialize new project

---
+++ 09-frontend-quiz-app/src/components/svgs/index.ts
@@ -0,0 +1,6 @@
+export { default as HTMLLogo } from "./HTMLLogo";
+export { default as CSSLogo } from "./CSSLogo";
+export { default as JavaScriptLogo } from "./JavaScriptLogo";
+export { default as Accessibility } from "./Accessibility";
+export { default as LightModeIcon } from "./LightModeIcon";
+export { default as DarkModeIcon } from "./DarkModeIcon";
---
+++ 09-frontend-quiz-app/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.tsx"],
+  theme: {
+    extend: {
+      theme: {
+        colors: {
+          purple: "#A729F5",
+          "dark-navy": "#313E51",
+          navy: "#3B4D66",
+          "gray-navy": "#626C7F",
+          "light-blue": "#ABC1E1",
+          "light-gray": "#F4F6FA",
+          white: "#FFFFFF",
+          green: "#26D782",
+          red: "#EE5454",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
