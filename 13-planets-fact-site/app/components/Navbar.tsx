d3e0973977ac93b19af6d7423af7bdc801e2a4c0
mamatsa <otar.mamatsashvili@gmail.com>
2024-02-03 14:00:46 +0400

    13 feat: style active navbar link

--- 13-planets-fact-site/app/components/Navbar.tsx
+++ 13-planets-fact-site/app/components/Navbar.tsx
@@ -1,34 +1,18 @@
-import Link from "next/link";
+import NavItem from "./NavItem";
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between border-b border-b-white border-opacity-20 px-6 py-4">
       <h1 className="text-2xl text-white">THE PLANETS</h1>
       <ul className="flex gap-8 text-sm font-bold tracking-widest text-white">
-        <li className="opacity-70 hover:opacity-100">
-          <Link href="/mercury">MERCURY</Link>
-        </li>
-        <li className="opacity-70 hover:opacity-100">
-          <Link href="/venus">VENUS</Link>
-        </li>
-        <li className="opacity-70 hover:opacity-100">
-          <Link href="/earth">EARTH</Link>
-        </li>
-        <li className="opacity-70 hover:opacity-100">
-          <Link href="/mars">MARS</Link>
-        </li>
-        <li className="opacity-70 hover:opacity-100">
-          <Link href="/jupiter">JUPITER</Link>
-        </li>
-        <li className="opacity-70 hover:opacity-100">
-          <Link href="/saturn">SATURN</Link>
-        </li>
-        <li className="opacity-70 hover:opacity-100">
-          <Link href="/uranus">URANUS</Link>
-        </li>
-        <li className="opacity-70 hover:opacity-100">
-          <Link href="/neptune">NEPTUNE</Link>
-        </li>
+        <NavItem href="/mercury" title="MERCURY" />
+        <NavItem href="/venus" title="VENUS" />
+        <NavItem href="/earth" title="EARTH" />
+        <NavItem href="/mars" title="MARS" />
+        <NavItem href="/jupiter" title="JUPITER" />
+        <NavItem href="/saturn" title="SATURN" />
+        <NavItem href="/uranus" title="URANUS" />
+        <NavItem href="/neptune" title="NEPTUNE" />
       </ul>
     </nav>
   );
